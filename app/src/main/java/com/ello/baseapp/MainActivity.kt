547e7d1e0c693c853375fc883995d51931d00b5a
Laros <laros01@hotmail.com>
2022-07-20 10:33:08 +0800

    Initial commit

---
+++ app/src/main/java/com/ello/baseapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ello.baseapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * @author dxl
+ * @date
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
